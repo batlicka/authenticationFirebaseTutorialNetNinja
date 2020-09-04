98e475965d7a4ccb106290b4c8cf1ee6dab46ab0
batlicka <vojta@vagunda.eu>
2020-09-04 11:24:45 +0200

    basic

---
+++ scripts/auth.js
@@ -0,0 +1,87 @@
+const pozdrav = (arg) => {
+  console.log("hello " + arg);
+};
+
+//GET DATA FROM FIRESTORE
+// db.collection('guides').get().then() .......je asynchronní task, může trvat různě dlouho, když je dokončen je vyhozena callback function
+//callback funkce vezme odpověď z db.collection('guides').get()
+
+// listen for auth status changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log("user is logged in as" + user.email);
+    db.collection("guides")
+      .get()
+      .then((snapshot) => {
+        setupGuides(snapshot.docs);
+      });
+    setupUI(user);
+  } else {
+    setupGuides([]);
+    setupUI();
+  }
+});
+
+//create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm['title'].value,
+    content: createForm['content'].value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
+
+// signup
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    console.log(email, password);
+    // close the signup modal & reset form
+    const modal = document.querySelector("#modal-signup");
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user loged out");
+  });
+});
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log("user" + cred.user.email + "was logged in");
+    // close the signup modal & reset form
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+// DOM elements
+const guideList = document.querySelector(".guides");
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+  if (user) {
+    // toggle user UI elements    
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // toggle user elements
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+};
+
+// setup guides
+const setupGuides = (data) => {  
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${
+            doc.data().title
+          } </div>
+          <div class="collapsible-body white"> ${doc.data().content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML = '<h5 class="center-align">Nothing to for view</h5>';
+  }
+};
+
+// setup materialize components
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(items);
+});
